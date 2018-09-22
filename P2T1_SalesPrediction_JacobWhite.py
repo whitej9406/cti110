f116b9efdef6c8b350805fd39e210033f03ddc7a
whitej9406 <43506036+whitej9406@users.noreply.github.com>
2018-09-22 19:02:52 -0400

    Add files via upload
    First Submission

---
+++ P2T1_SalesPrediction_JacobWhite.py
@@ -0,0 +1,14 @@
+# Get the projected total sales.
+# 9/22/2018
+# CTI-110 P2T1 - Sales Prediction
+# Jacob White
+#
+total_sales = float(input('Enter the projected sales: '))
+
+# Calculate the profit as 23 percent of total sales.
+profit = total_sales * 0.23
+
+# Display the profit.
+print('The profit is $', format(profit, ',.2f'))
+
+
